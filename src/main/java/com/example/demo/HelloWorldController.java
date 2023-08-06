c3c8db990ddd6cf86f2e5e0b14ab804835d517d3
enrique.pineda <enriquepineda@wecancodeit.org>
2023-08-06 12:57:35 -0400

    from original project

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+   // this message we are reading from application.properties file
+    @Value("${hello.message}")
+    private String message = "Sup.";
+
+    @GetMapping("/")
+    public String hellomessage(Model model) {
+        model.addAttribute("message", message);
+        return "helloworld"; //view name
+    }
+// pass some parameter like /hello?name=thymeleaf
+    @GetMapping("/hello")
+    public String helloWithParam(
+        @RequestParam(name = "name", required = true, defaultValue = "springboot")
+        String name, Model model) {
+        model.addAttribute("message", name);
+        return "helloworld"; //view name
+    } 
+}
